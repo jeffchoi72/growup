6f8f67c54bdce4d4c246789a1410a7ece1f311ee
jeffchoi72 <ihello0720@gmail.com>
2019-07-01 23:51:28 +0900

    server: init grwoup-server

---
+++ growup-server/src/router/index.ts
@@ -0,0 +1,15 @@
+import { Context } from 'koa';
+import * as Router from 'koa-router';
+
+import { TestCtrl } from '../controller';
+
+const rootRouter: Router = new Router();
+
+rootRouter.get('/test', TestCtrl.getText);
+
+rootRouter.all('*', (ctx: Context) => {
+  ctx.status = 404;
+  ctx.body = '존재하지 않는 API 입니다';
+});
+
+export default rootRouter;
