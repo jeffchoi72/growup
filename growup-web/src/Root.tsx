7009b9893c4be67af5b22ee1a5025adaf1c80a1d
jeffchoi72 <ihello0720@gmail.com>
2019-07-05 23:19:16 +0900

    web: apply react-router

--- growup-web/src/Root.tsx
+++ growup-web/src/Root.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+import RootRouter from './domain/RootRouter';
 
 const Root: React.FC = () => {
-  return <div>Root</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <RootRouter />
+    </>
+  );
 };
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 export default Root;
---
+++ growup-web/src/domain/RootRouter.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const RootRouter: React.FC = () => {
+  return <BrowserRouter>Route들을 import 해서 사용하면 될 듯</BrowserRouter>;
+};
+
+export default RootRouter;
