1005c3bb6add40583972eb409524e7343695fc51
jeffchoi72 <ihello0720@gmail.com>
2019-08-21 23:02:16 +0900

    install(web): install redux and other library

--- growup-web/src/Root.tsx
+++ growup-web/src/Root.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import RootRouter from './pages';
 import GlobalStyle from './styles/globalStyles';
 
 const Root: React.FC = () => {
   return (
-    <>
+    <Provider store={{} as any}>
       <GlobalStyle />
       <RootRouter />
-    </>
+    </Provider>
   );
 };
 
