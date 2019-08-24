f56f3da6bcf6633c52f3f50bb52496ffa4075aec
jeffchoi72 <ihello0720@gmail.com>
2019-08-24 23:43:15 +0900

    feat(web): add browserStore utils

---
+++ growup-web/src/common/utils/browserStore.ts
@@ -0,0 +1,32 @@
+const BrowserStore = () => {
+  const store = localStorage || {};
+
+  return {
+    set: (key: string, object: any) => {
+      store[key] = typeof object === "string" ? object : JSON.stringify(object);
+    },
+    get: (key: string) => {
+      if (!store[key]) {
+        return null;
+      }
+
+      const value = store[key];
+
+      try {
+        const parsed = JSON.parse(value);
+        return parsed;
+      } catch (error) {
+        return value;
+      }
+    },
+    remove: (key: string) => {
+      if (localStorage) {
+        return localStorage.removeItem(key);
+      }
+
+      delete store[key];
+    }
+  };
+};
+
+export default BrowserStore();
